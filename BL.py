26d9ce1f3eee4612b55a4f87eb974e8f9b319c8b
GIRI NARASIMHA T <46864302+GIRINARASIMHAT@users.noreply.github.com>
2020-10-20 17:13:22 +0530

    Update BL.py

--- BL.py
+++ BL.py
@@ -1,21 +1,12 @@
 #! python3
 
-
 # Program to Add Bullets to the Text
 
-
-import pyperclip                # importing the module pyperclip.
-
-text = pyperclip.paste()        # The text is copied from the computer's clipBoard into the variable called 'text'.
-
-lines = text.split('\n')        # Splitting the text into lines, when we find a newline delimitter[\n] and storing into a variable 'lines'.
-
-for i in range(len(lines)):     # Adding Bullets[*] to all the lines by accessing the each line using a 'for' loop.
+import pyperclip               
+text = pyperclip.paste()      
+lines = text.split('\n')  
+for i in range(len(lines)):    
     lines[i] = '* ' + lines[i]  
-    
-text = '\n'.join(lines)         # Now the lines are stored back to 'text' variable with newline delimitter[\n].
-
-pyperclip.copy(text)            # Finally pasting the text back to the Computer's clipboard.
-
-
+text = '\n'.join(lines)      
+pyperclip.copy(text)           
 print('Text is added with Bullets[*].')
